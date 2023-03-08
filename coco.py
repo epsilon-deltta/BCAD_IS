6b83c6701f2f3dec068fdbc6f9c8c1309b9c0878
epsilon-deltta <kokomong1316@gmail.com>
2023-03-08 20:33:30 +0900

    feat: copy paste code

---
+++ coco.py
@@ -0,0 +1,193 @@
+import os
+import cv2
+from torchvision.datasets import CocoDetection
+from copy_paste import copy_paste_class
+
+min_keypoints_per_image = 10
+
+def _count_visible_keypoints(anno):
+    return sum(sum(1 for v in ann["keypoints"][2::3] if v > 0) for ann in anno)
+
+def _has_only_empty_bbox(anno):
+    return all(any(o <= 1 for o in obj["bbox"][2:]) for obj in anno)
+
+def has_valid_annotation(anno):
+    # if it's empty, there is no annotation
+    if len(anno) == 0:
+        return False
+    # if all boxes have close to zero area, there is no annotation
+    if _has_only_empty_bbox(anno):
+        return False
+    # keypoints task have a slight different critera for considering
+    # if an annotation is valid
+    if "keypoints" not in anno[0]:
+        return True
+    # for keypoint detection tasks, only consider valid images those
+    # containing at least min_keypoints_per_image
+    if _count_visible_keypoints(anno) >= min_keypoints_per_image:
+        return True
+
+    return False
+
+@copy_paste_class
+class CocoDetectionCP(CocoDetection):
+    def __init__(
+        self,
+        root,
+        annFile,
+        transforms
+    ):
+        super(CocoDetectionCP, self).__init__(
+            root, annFile, None, None, transforms
+        )
+
+        # filter images without detection annotations
+        ids = []
+        for img_id in self.ids:
+            ann_ids = self.coco.getAnnIds(imgIds=img_id, iscrowd=None)
+            anno = self.coco.loadAnns(ann_ids)
+            if has_valid_annotation(anno):
+                ids.append(img_id)
+        self.ids = ids
+
+    def load_example(self, index):
+        img_id = self.ids[index]
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+        target = self.coco.loadAnns(ann_ids)
+
+        path = self.coco.loadImgs(img_id)[0]['file_name']
+        image = cv2.imread(os.path.join(self.root, path))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        #convert all of the target segmentations to masks
+        #bboxes are expected to be (y1, x1, y2, x2, category_id)
+        masks = []
+        bboxes = []
+        for ix, obj in enumerate(target):
+            masks.append(self.coco.annToMask(obj))
+            bboxes.append(obj['bbox'] + [obj['category_id']] + [ix])
+        
+        #pack outputs into a dict
+        output = {
+            'image': image,
+            'masks': masks,
+            'bboxes': bboxes
+        }
+        
+        return self.transforms(**output)
+
+
+import os
+from PIL import Image
+import torch
+import numpy as np
+import random
+import albumentations as A
+class CocoDetectionCP_0(CocoDetection):
+    def __init__(
+        self,
+        root,
+        annFile,
+        transforms
+    ):
+        super().__init__(
+            root, annFile, None, None, transforms
+        )
+
+        # filter images without detection annotations
+        ids = []
+        for img_id in self.ids:
+            ann_ids = self.coco.getAnnIds(imgIds=img_id, iscrowd=None)
+            anno = self.coco.loadAnns(ann_ids)
+            if has_valid_annotation(anno):
+                ids.append(img_id)
+        
+        self.ids = ids 
+        self._split_transforms()
+
+    def _split_transforms(self):
+        split_index = None
+        for ix, tf in enumerate(list(self.transforms.transforms)):
+            if tf.get_class_fullname() == 'copypaste.CopyPaste':
+                split_index = ix
+
+        if split_index is not None:
+            tfs = list(self.transforms.transforms)
+            pre_copy = tfs[:split_index]
+            copy_paste = tfs[split_index]
+            post_copy = tfs[split_index+1:]
+
+            #replicate the other augmentation parameters
+            bbox_params = None
+            keypoint_params = None
+            paste_additional_targets = {}
+            if 'bboxes' in self.transforms.processors:
+                bbox_params = self.transforms.processors['bboxes'].params
+                paste_additional_targets['paste_bboxes'] = 'bboxes'
+                if self.transforms.processors['bboxes'].params.label_fields:
+                    msg = "Copy-paste does not support bbox label_fields! "
+                    msg += "Expected bbox format is (a, b, c, d, label_field)"
+                    raise Exception(msg)
+            if 'keypoints' in self.transforms.processors:
+                keypoint_params = self.transforms.processors['keypoints'].params
+                paste_additional_targets['paste_keypoints'] = 'keypoints'
+                if keypoint_params.label_fields:
+                    raise Exception('Copy-paste does not support keypoint label fields!')
+
+            if self.transforms.additional_targets:
+                raise Exception('Copy-paste does not support additional_targets!')
+
+            #recreate transforms
+            self.transforms = A.Compose(pre_copy, bbox_params, keypoint_params, additional_targets=None)
+            self.post_transforms = A.Compose(post_copy, bbox_params, keypoint_params, additional_targets=None)
+            self.copy_paste = A.Compose(
+                [copy_paste], bbox_params, keypoint_params, additional_targets=paste_additional_targets
+            )
+        else:
+            self.copy_paste = None
+            self.post_transforms = None
+            
+    def load_example(self, index):
+        img_id = self.ids[index]
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+        target = self.coco.loadAnns(ann_ids)
+
+        path = self.coco.loadImgs(img_id)[0]['file_name']
+        image = cv2.imread(os.path.join(self.root, path))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        #convert all of the target segmentations to masks
+        #bboxes are expected to be (y1, x1, y2, x2, category_id)
+        masks = []
+        bboxes = []
+        for ix, obj in enumerate(target):
+            masks.append(self.coco.annToMask(obj))
+            bboxes.append(obj['bbox'] + [obj['category_id']] + [ix])
+        
+        #pack outputs into a dict
+        output = {
+            'image': image,
+            'masks': masks,
+            'bboxes': bboxes
+        }
+        
+        return self.transforms(**output)
+    
+    def __getitem__(self, idx):  
+        #split transforms if it hasn't been done already
+        if not hasattr(self, 'post_transforms'):
+            self._split_transforms()
+
+        img_data = self.load_example(idx)
+        if self.copy_paste is not None:
+            paste_idx = random.randint(0, self.__len__() - 1)
+            paste_img_data = self.load_example(paste_idx)
+            for k in list(paste_img_data.keys()):
+                paste_img_data['paste_' + k] = paste_img_data[k]
+                del paste_img_data[k]
+            
+            img_data = self.copy_paste(**img_data, **paste_img_data)
+            img_data = self.post_transforms(**img_data)
+            img_data['paste_index'] = paste_idx 
+        
+        return img_data
